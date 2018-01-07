ff510a7e7e8109aa171136d5c14a6da261e47984
Sam Gauck <21samuelg@abingtonfriends.net>
2018-01-07 09:02:40 -0500

    Create Xcode project

---
+++ Tactics/Tactics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tactics
+//
+//  Created by Sam Gauck on 1/7/18.
+//  Copyright © 2018 Sam Gauck. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tactics/Tactics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tactics
+//
+//  Created by Sam Gauck on 1/7/18.
+//  Copyright © 2018 Sam Gauck. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
