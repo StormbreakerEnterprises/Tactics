cd50ec6921045f450c6bf6852547025ea5317f46
Sam Gauck <21samuelg@abingtonfriends.net>
2018-01-17 19:28:11 -0500

    Start writing Unit.swift

---
+++ Tactics/Tactics/Unit.swift
@@ -0,0 +1,20 @@
+//
+//  Unit.swift
+//  Tactics
+//
+//  Created by Sam Gauck on 1/17/18.
+//  Copyright © 2018 Sam Gauck. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+class Unit: NSButton {
+    var x: CGFloat {
+        get {
+            return self.frame.origin.x
+        }
+        set(newX) {
+            
+        }
+    }
+}
