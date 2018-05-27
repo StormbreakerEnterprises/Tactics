0d973823b0f449d1a19ab4d8f383a715ec7678e7
Sam Gauck <21samuelg@abingtonfriends.net>
2018-05-27 18:25:34 -0400

    Replace Tactics with empty SpriteKit project

--- Tactics/Tactics/AppDelegate.swift
+++ Tactics/Tactics/AppDelegate.swift
@@ -2,26 +2,24 @@
 //  AppDelegate.swift
 //  Tactics
 //
-//  Created by Sam Gauck on 1/7/18.
+//  Created by Sam Gauck on 5/27/18.
 //  Copyright © 2018 Sam Gauck. All rights reserved.
 //
+
 
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
-
---
+++ Tactics/Tactics/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Tactics
+//
+//  Created by Sam Gauck on 5/27/18.
+//  Copyright © 2018 Sam Gauck. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        //setup
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
